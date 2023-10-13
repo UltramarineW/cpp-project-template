c674eaa98d8daaa3201ebf717941f2dcf384b933
UltramarineW <2021113679@stu.hit.edu.cn>
2023-10-13 18:54:14 +0800

    Init Repo

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by wujiayang on 2023/10/13.
+//
+#include "gflags/gflags.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
+
+int main(int argc, char*argv[]) {
+    // initialize gflags
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    // set spdlog display level (trace debug info warn err critical off)
+    // spdlog::level::info for not display trace and debug
+    spdlog::set_level(spdlog::level::info);
+
+    int a = 0;
+    spdlog::info("Hello world for {}", a);
+    return 0;
+}
---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by wujiayang on 2023/10/13.
+//
+#include "gtest/gtest.h"
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
